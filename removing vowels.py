e775ffc1cd70df4976522f515c7739cd249ec74c
DavidMwangi <david.mathenge98@gmail.com>
2017-03-31 13:13:00 +0300

    Removes all vowels from a word r sentence

--- removing vowels.py
+++ removing vowels.py
@@ -6,6 +6,10 @@ output = ""
 
 for letter in prompt:
 
+    if letter in vowels:
+
+        print("Found " + letter + " removing " + letter)
+
     if letter not in vowels:
 
         output += letter
